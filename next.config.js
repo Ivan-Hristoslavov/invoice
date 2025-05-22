7be489ac4ef0f525a9251add6033a90ae1710fbe
Ivan Ivanov <ivanivanov@Ivans-MacBook-Pro.local>
2025-05-22 14:44:12 +0300

    feat

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: '.next',
+  output: 'standalone',
   images: {
     domains: ['res.cloudinary.com'], // Ако използвате Cloudinary за изображения
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
